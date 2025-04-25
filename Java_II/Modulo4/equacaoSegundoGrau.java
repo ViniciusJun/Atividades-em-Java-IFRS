efa3baba099845b6d756c5b4271af0e6a618ae94
Vinicius Pereira de Jesus Junior <167610575+ViniciusJun@users.noreply.github.com>
2025-04-25 14:21:51 -0300

    atividade 4

---
+++ Java_II/Modulo4/equacaoSegundoGrau.java
@@ -0,0 +1,15 @@
+package Modulo4;
+
+public class equacaoSegundoGrau {
+
+    public static void main(String[] args){
+        System.out.println("Equações de Sedundo Grau\ln")
+        double A = Double.parseDouble(System.console().readLine("Informe o valor de A: "));
+        double B = Double.parseDouble(System.console().readLine("Informe o valor de B: "));
+        double C = Double.parseDouble(System.console().readLine("Informe o valor de C: "));
+
+        double delta;
+
+        delta = 
+    }
+}
